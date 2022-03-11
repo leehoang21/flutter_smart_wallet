b17eee075c44219faa8af2a5ce68a0514f90cf10
= <=>
2022-03-11 18:37:39 +0700

    decoupling from DioApiClient

--- lib/repository/remote/vn_bank_repository.dart
+++ lib/repository/remote/vn_bank_repository.dart
@@ -4,6 +4,10 @@ import 'package:flutter_smart_wallet/common/constants/api_path_constants.dart';
 import 'package:flutter_smart_wallet/common/enums/app_enums.dart';
 
 class VnBankRepository {
+  final DioApiClient _dioApiClient;
+
+  VnBankRepository(this._dioApiClient);
+
   Future<BaseResponse> getVnBankList() async {
     return DioApiClient()
         .request(method: NetworkMethod.get, url: ApiPathConstants.getVnBanks);
