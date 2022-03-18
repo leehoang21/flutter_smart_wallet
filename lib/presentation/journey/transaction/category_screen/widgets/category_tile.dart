1a04ef6567ee9563d95d00889b2024ba7a1a022d
= <=>
2022-03-18 11:06:34 +0700

    remove unecesssary imports

--- lib/presentation/journey/transaction/category_screen/widgets/category_tile.dart
+++ lib/presentation/journey/transaction/category_screen/widgets/category_tile.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_smart_wallet/model/category_model.dart';
