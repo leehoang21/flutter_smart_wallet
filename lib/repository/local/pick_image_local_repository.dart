30bcda5e5bb7aae9c059cf5405e4de57b8be4830
Thanhbt1811 <bt.thanh1811@gmail.com>
2022-03-20 18:18:01 +0700

    thanhbt-refactor: register upload image with getit and remote import not use

--- lib/repository/local/pick_image_local_repository.dart
+++ lib/repository/local/pick_image_local_repository.dart
@@ -2,7 +2,9 @@ import 'dart:io';
 import 'dart:typed_data';
 import 'package:flutter_smart_wallet/common/utils/compress.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:injectable/injectable.dart';
 
+@injectable
 class PickImageLocalRepository {
   ImagePicker imagePicker = ImagePicker();
 
