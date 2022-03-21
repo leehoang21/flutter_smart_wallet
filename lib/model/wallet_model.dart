d95249a915a91f364b73c2e2249cfbce75824e81
Tran Truc Diep <trucdiep2626@gmail.com>
2022-03-21 22:38:10 +0700

    update image 2x 3x, add function toJson and fromJson

--- lib/model/wallet_model.dart
+++ lib/model/wallet_model.dart
@@ -2,10 +2,7 @@ import 'package:flutter_smart_wallet/common/configs/hive_config/hive_type_id.dar
 import 'package:hive/hive.dart';
 import 'package:json_annotation/json_annotation.dart';
 
-part 'wallet_model.g.dart';
-
-@JsonSerializable()
-@HiveType(typeId :HiveTypeId.userType)
+@HiveType(typeId: HiveTypeId.userType)
 class WalletModel {
   @HiveField(0)
   final String? walletImage;
@@ -20,18 +17,34 @@ class WalletModel {
   @HiveField(5)
   final int? lastUpdate;
 
-
-  WalletModel({this.walletImage, this.walletName, this.balance, this.walletType,
-    this.createAt, this.lastUpdate});
+  WalletModel(
+      {this.walletImage,
+      this.walletName,
+      this.balance,
+      this.walletType,
+      this.createAt,
+      this.lastUpdate});
 
   @override
   String toString() {
     return 'WalletModel{walletImage: $walletImage, walletName: $walletName, balance: $balance, walletType: $walletType, createAt: $createAt, lastUpdate: $lastUpdate}';
   }
 
-  factory WalletModel.fromJson(Map<String, dynamic> json) =>
-      _$WalletModelFromJson(json);
+  factory WalletModel.fromJson(Map<String, dynamic> json) => WalletModel(
+        walletImage: json['walletImage'] as String?,
+        walletName: json['walletName'] as String?,
+        balance: json['balance'] as int?,
+        walletType: json['walletType'] as int?,
+        createAt: json['createAt'] as int?,
+        lastUpdate: json['lastUpdate'] as int?,
+      );
 
-  Map<String, dynamic> toJson() => _$WalletModelToJson(this);
-
+  Map<String, dynamic> toJson() => <String, dynamic>{
+        'walletImage': walletImage,
+        'walletName': walletName,
+        'balance': balance,
+        'walletType': walletType,
+        'createAt': createAt,
+        'lastUpdate': lastUpdate,
+      };
 }
